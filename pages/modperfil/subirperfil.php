ca75f57a466fe25e2fc21d906ba4a88ec9e83bc1
Patricio Gallo <patogallo95@gmail.com>
2022-04-29 17:54:13 -0300

    up to date

--- pages/modperfil/subirperfil.php
+++ pages/modperfil/subirperfil.php
@@ -3,12 +3,22 @@ session_start();
 include("../../config/db.php"); //incluir database
 include("../php/itemslogin.php"); //incluir items del login
 
+
 if($_SESSION["autorizado"] == TRUE){
 
 include("../php/verificar.php"); //verificacion de inicio de secion y caducidad
 
+$subido=1;
+
+if($_FILES){
+
+$ruta_fotoSubida = $_FILES['perfil']['tmp_name'];
+move_uploaded_file($ruta_fotoSubida,"../user/".$id."/perfil/perfil.jpg");
+$subido=0;
+}
 
  ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,10 +32,11 @@ include("../php/verificar.php"); //verificacion de inicio de secion y caducidad
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
-  <title>UPLOADER / Editar Perfil</title>
+  <title>UPLOADER / editar perfil</title>
 </head>
 
 <body>
+
   <header>
     <div class="header_titulo">
       <h1> <a href="../inicio.php"> Uploader </a></h1>
@@ -36,54 +47,42 @@ include("../php/verificar.php"); //verificacion de inicio de secion y caducidad
   <div class="cuerpo">
 
 
-    <div class="cuerpo_portada">
-
-      <div class="imagen_portada">
-        <img src="../user/<?php echo $id."/perfil/portada2.jpg"?>" alt="">
+    <div class="cuerpo_publicacion2">
+      <div class="cuerpo_publicacion2Titutlo">
+        <h1>Editar foto de perfil</h1>
       </div>
-      <div class="portada_perfil">
-        <img src="../user/<?php echo $id."/perfil/perfil.jpg"?>" alt="">
-      </div>
-    </div>
-
-
-    <div class="cuerpo_debajoPortada">
 
       <div class="cuerpo_linea"></div>
+      <form class="" action="subirperfil.php" method="post" enctype="multipart/form-data">
 
-      <div class="cuerpo_editarportada">
-        <h1>Aqui puedes editar tus fotos de perfil o portada</h1>
-      </div>
-      <div class="cuerpo_linea"></div>
-      <div class="cuerpo_botonesDeEdicion">
-      <a href="subirperfil.php"> <button type="button" name="button">Foto perfil</button></a>
-      <a href="subirportada.php">  <button type="button" name="button">Foto portada</button></a>
-      </div>
+        <div class="cuerpo_publicacionPerfil">
+          <a href="perfil.html"><img src="../user/<?php echo $id."/perfil/perfil.jpg"?>" alt=""></a>
+        </div>
+        <div class="cuerpo_linea"></div>
+        <div class="publicacion2_formulario">
+
+          <h1>Aqui puedes seleccionar tu nueva foto de perfil</h1>
+
+          <?php if($subido==1){ ?>
+          <input type="file" name="perfil" value="">
+        <!--  <button type="button" name="button" id="subir">Seleccionar</button> -->
+<?php }else if($subido==0){
+  echo "La foto de perfil se guardo con exito";
+} ?>
+          <div class="cuerpo_linea"></div>
+          <button type="submit" name="button">Subir</button>
 
 
-
-
-
-
-
-
-
-
+      </form>
     </div>
-
-
-
 
   </div>
 
-
-
+  </div>
 
 </body>
 
 </html>
-
-
 <?php }
 
 else {
